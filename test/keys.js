cea11bf70824a0bc0dd68bd6a4aaec34668795aa
Maurits Rijk <maurits.rijk@ing.nl>
2017-01-27 11:10:16 +0100

    Complete test

--- test/keys.js
+++ test/keys.js
@@ -7,15 +7,19 @@ const s = require('../spec');
 const {isString} = require('./utils');
 
 describe('Test the keys function', () => {
+    s.def('::email-type', s.and(isString));
+    
     s.def('::first-name', isString);
-    s.def('::lastt-name', isString);
-    s.def('::person', s.keys({}));
+    s.def('::last-name', isString);
+    s.def('::email', '::email-type');
+    s.def('::person', s.keys({req: ['::first-name', '::last-name', ':email:'], opt: []}));
     
     it('should return the value', () => {
         expect(s.isValid('::person',
                          {
                              '::first-name': 'Elon',
-                             '::last-name': "Musk"
+                             '::last-name': 'Musk',
+                             '::email': 'elon@example.com'
                          })).to.be.true;
     });
 });
