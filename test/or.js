e188f1b8dfe28384f27ddb58123de602ed06b709
Maurits Rijk <maurits.rijk@ing.nl>
2017-01-27 15:59:34 +0100

    Annotate or spec

--- test/or.js
+++ test/or.js
@@ -7,6 +7,9 @@ const s = require('../spec');
 const {isInt, isInteger, isString} = require('./utils');
 
 describe('Test the or function', () => {
+
+    s.def('::name-or-id', s.or(':name', isString, ':id', isInt));
+
     it('should test the or of 1 spec', () => {
         s.def('::integer?', s.or(':x', isInteger));
         expect(s.isValid('::integer?', 12)).to.be.true;
@@ -14,9 +17,12 @@ describe('Test the or function', () => {
     });
     
     it('should test the or of 2 specs', () => {
-        s.def('::name-or-id', s.or(':name', isString, ':id', isInt));
         expect(s.isValid('::name-or-id', 'abc')).to.be.true;
         expect(s.isValid('::name-or-id', 100)).to.be.true;
         expect(s.isValid('::name-or-id', 3.14)).to.be.false;
+    });
+
+    it('should return tag name and conformed value', () => {
+        expect(s.conform('::name-or-id', 'abc')).to.equal([]);
     });
 });
