823eadcc0c9d91739aaf8646d8f8f7d3cc8768d6
Maurits <maurits.rijk@gmail.com>
2017-01-28 20:37:56 +0100

    More generator stuff

--- test/gen.js
+++ test/gen.js
@@ -2,7 +2,7 @@
 
 const {expect} = require('chai')
 
-const {isInteger} = require('./utils');
+const {isInt, isString} = require('./utils');
 
 const s = require('../lib/spec');
 const gen = require('../lib/gen');
@@ -10,6 +10,14 @@ const gen = require('../lib/gen');
 describe('Test node.spec.gen functions', () => {
 
     it('should generate a single integer', () => {
-        expect(isInteger(gen.generate(s.gen(isInteger)))).to.be.true;
+        expect(isInt(gen.generate(s.gen(isInt)))).to.be.true;
+    });
+
+    it('should generate a single string', () => {
+        expect(gen.generate(s.gen(isString))).to.be.a('string');
+    });
+
+    it('should generate 10 integers', () => {
+        expect(gen.sample(s.gen(isInt))).to.have.length(10);
     });
 });
