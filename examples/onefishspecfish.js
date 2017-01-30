8f3d569b5ec65a472c414878781fb72fc9228f10
Maurits <maurits.rijk@gmail.com>
2017-01-30 20:21:34 +0100

    Add another example

---
+++ examples/onefishspecfish.js
@@ -0,0 +1,7 @@
+// Example: One Fish Spec Fish. Node.spec implementation
+// of http://gigasquidsoftware.com/blog/2016/05/29/one-fish-spec-fish/
+// Blog by Carin Meier
+
+const _ = require('lodash');
+
+const s = require('../lib/spec');
