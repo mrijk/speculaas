3442b205f77055878b807edb59607366d66a740f
Maurits Rijk <maurits.rijk@ing.nl>
2017-01-31 13:02:00 +0100

    Throw error when spec doesnt exist

--- test/spec.js
+++ test/spec.js
@@ -19,32 +19,6 @@ describe('Test node.spec functions', () => {
     describe('Test the alt function', () => {
     });
 
-    describe('Test the isValid function', () => {
-            
-        it('should return true when a value meets a spec', () => {
-            s.def('::a', isInteger);
-            expect(s.isValid('::a', 12)).to.be.true;        
-        });
-        
-        it('should return false when a value doesn\'t meet a spec', () => {
-            s.def('::a', isInteger);
-            expect(s.isValid('::a', 'foobar')).to.be.false;        
-        });
-
-        it('should accept a function', () => {
-            expect(s.isValid(isOdd, 5)).to.be.true;
-        });
-
-        it('should accept a lambda expression', () => {
-            expect(s.isValid(x => x > 5, 0)).to.be.false;
-        });
-
-        it('should accept an array', () => {
-            expect(s.isValid(suit, ':club')).to.be.true;
-            expect(s.isValid(suit, 42)).to.be.false;
-        });
-    });
-
     describe('Test the getSpec function', () => {
         it('should return an existing spec', () => {
             s.def('::odd?', s.and(isInteger, isOdd));
