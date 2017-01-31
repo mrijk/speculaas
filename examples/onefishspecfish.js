c1f873e6e8ebc1a59bf468ace1e93f793b39c06e
Maurits Rijk <maurits.rijk@ing.nl>
2017-01-31 08:37:48 +0100

    Implement more of One Fish Spec Fish blog

--- examples/onefishspecfish.js
+++ examples/onefishspecfish.js
@@ -5,3 +5,20 @@
 const _ = require('lodash');
 
 const s = require('../lib/spec');
+
+const fishNumbers = {0: 'Zero',
+                     1: 'One',
+                     2: 'Two'};
+
+s.def('::fish-number', _.keys(fishNumbers).map(_.toInteger));
+
+s.isValid('::fish-number', 1);
+s.isValid('::fish-number', 5);
+
+s.explain('::fish-number', 5);
+
+s.def('::color', ['Red', 'Blue', 'Dun']);
+
+s.def('::first-line', s.cat(':n1', '::fish-number', ':n2', '::fish-number', 'c1', '::color', 'c2', '::color'));
+
+s.explain('::first-line', [1, 2, 'Red', 'Black']);
