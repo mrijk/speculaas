997f89882f34cb9f0840101fc0d5307164df8f25
Maurits Rijk <maurits.rijk@ing.nl>
2017-02-07 12:51:53 +0100

    Start with describe implementation

---
+++ test/describe.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {expect} = require('chai');;
+
+const s = require('../lib/spec');
+
+describe('Test the describe function', () => {
+    
+    it('should describe a ...', () => {
+    });
+});
+
