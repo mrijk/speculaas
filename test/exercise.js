ae26a3ca86edb7d552add10311f4a68563895f49
Maurits Rijk <maurits.rijk@ing.nl>
2017-02-09 11:16:08 +0100

    Fix generator

--- test/exercise.js
+++ test/exercise.js
@@ -7,7 +7,7 @@ const s = require('../lib/spec');
 const {isInteger} = require('./utils');
 
 describe('Test the exercise function', () => {
-    it.only('should generate 10 integers', () => {
+    it('should generate 10 integers', () => {
         console.log(s.exercise(isInteger));
     });
 });
