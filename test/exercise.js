5e0d39a6c8a4eda4b106dba470e7d5830e8d68d4
Maurits Rijk <maurits.rijk@ing.nl>
2017-02-10 15:41:53 +0100

    improvements

--- test/exercise.js
+++ test/exercise.js
@@ -4,15 +4,20 @@ const {expect} = require('chai');;
 
 const s = require('../lib/spec');
 
-const {isInteger} = require('./utils');
+const {isInteger, isString} = require('./utils');
 
 describe('Test the exercise function', () => {
     it('should generate 10 integers', () => {
         expect(s.exercise(isInteger)).to.have.length(10);
     });
 
-    it('should generate 10 random values from a set', () => {
+    it('should generate 5 random values from a set', () => {
         s.def('::color', ['Red', 'Blue', 'Dun']);
-        console.log(s.exercise('::color'));
-    });    
+        expect(s.exercise('::color', 5)).to.have.length(5);
+    });
+
+    it.only('should generate 7 random values from a spec', () => {
+        s.def('::pairs', s.cat(':n', isInteger, ':s', isString));
+        console.log(s.exercise('::pairs', 7));
+    });
 });
