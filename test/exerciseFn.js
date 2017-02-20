9439c0eb70d98c60025a2d30e064fcac2d241a2d
Maurits Rijk <maurits.rijk@ing.nl>
2017-02-20 14:12:05 +0100

    Start exerciseFn implementation

--- test/exerciseFn.js
+++ test/exerciseFn.js
@@ -4,7 +4,15 @@ const {expect} = require('chai');;
 
 const s = require('../lib/spec');
 
+const {isInteger} = require('./utils');
+
 describe('Test the exerciseFn function', () => {
-    it('should do something...', () => {
+    it.only('call function', () => {
+        const square = x => x * x;
+        s.fdef(square, {
+            args: s.cat(':x', isInteger)
+        });
+
+        console.log(s.exerciseFn(square));
     });
 });
