8e5e69ff23187b4ddc36ecfff4da7aac9345a2e1
Maurits Rijk <maurits.rijk@ing.nl>
2017-02-24 16:23:53 +0100

    Fix more stuff

--- test/mapOf.js
+++ test/mapOf.js
@@ -33,9 +33,9 @@ describe('Test the mapOf function', () => {
         expect(s.isValid('::scores', map)).to.be.false;
     });
 
-    it.only('should implement a generator', () => {
-        console.log(s.exercise('::scores'));
-//        expect(s.exercise(s.collOf(isString))).to.have.length(10)
+    it('should implement a generator', () => {
+//        console.log(s.exercise('::scores'));
+        expect(s.exercise('::scores')).to.have.length(10);
 //            .to.satisfy(sample => _.every(sample, ([v]) => _.isArray(v) && v.length === 2));
     });
 });
