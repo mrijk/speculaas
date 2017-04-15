b6da9f58bd7eae9e4ad2ade9e6cc31200dfeb0bf
Maurits <maurits.rijk@gmail.com>
2017-04-15 18:25:07 +0200

    Add test case

--- test/exercise.js
+++ test/exercise.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const _ = require('lodash');
 
 const {expect} = require('chai');;
@@ -30,5 +28,10 @@ describe('Test the exercise function', () => {
 
     it('should generate 10 random values from a spec object', () => {
         expect(s.exercise(s.nilable(isString))).to.have.length(10).to.satisfy(sample => _.every(sample, ([s]) => isNull(s) || isString(s)));
+    });
+
+    it('should throw an exception', () => {
+        s.def('::impossible', s.and(isInteger, isString));
+        expect(() => s.exercise('::impossible')).to.throw(Error, /sampleFromSpec failed.*/);
     });
 });
