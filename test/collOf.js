5af6ef8d4f9a6c1d2eef5bdf0c3cff78ff65d3a7
Maurits Rijk <maurits.rijk@ing.nl>
2017-04-19 15:17:33 +0200

    Updates

--- test/collOf.js
+++ test/collOf.js
@@ -10,29 +10,36 @@ describe('Test the collOf function', () => {
     before(() => {
         s.def('::vnum3', s.collOf(isNumber, {kind: isVector, count: 3, distinct: true}));
     });
-    
+
     it('should return a spec for a collection', () => {
         expect(s.isValid('::vnum3', [1, 2, 3])).to.be.true;
     });
-    
+
     it('should fail on invalid type (not a vector)', () => {
         expect(s.isValid('::vnum3', {x: 1, y: 2, z: 3})).to.be.false;
     });
-    
+
     it('should fail on non-distinct data', () => {
         expect(s.isValid('::vnum3', [1, 1, 1])).to.be.false;
     });
-    
+
     it('should fail on invalid data', () => {
         expect(s.isValid('::vnum3', [1, 2, 'foo'])).to.be.false;
     });
-    
+
     it('should fail on invalid length', () => {
         expect(s.isValid('::vnum3', [1, 2, 3, 4])).to.be.false;
     });
 
     it('should conform to a valid value',  () => {
         expect(s.conform(s.collOf(isNumber), [5, 10, 2])).to.eql([5, 10, 2]);
+    });
+
+    it('should unform a conformed value', () => {
+        s.def('::vnum', s.collOf(isNumber));
+        const input = [5, 10, 2];
+        const conformed = s.conform("::vnum", input);
+        expect(s.unform('::vnum', conformed)).to.eql(input);
     });
 
     it('should have a minCount of 3', () => {
@@ -51,7 +58,7 @@ describe('Test the collOf function', () => {
         s.def('::vnum', s.collOf(isNumber, {into: new Set()}));
         expect(s.conform('::vnum', [1, 2, 3])).to.be.an.instanceof(Set);
     });
-    
+
     it('should accept an object of type Set as input', () => {
         s.def('::vset3', s.collOf(isNumber, {kind: isSet, count: 3}));
         expect(s.isValid('::vset3', new Set([1, 2, 3]))).to.be.true;
