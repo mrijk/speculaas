9560d6eb29d7ebac4574ded5463cb0c13c6a8c55
Maurits Rijk <maurits.rijk@ing.nl>
2017-04-19 09:50:33 +0200

    Add some more documentation and implent describe for arrays

---
+++ docs/api/describe.md
@@ -0,0 +1,15 @@
+describe
+=====
+
+Usage: ```describe(spec)```
+
+Returns an abbreviated description of the spec as data.
+
+[Source](https://github.com/mrijk/speculaas/blob/master/lib/describe.js)
+
+Example:
+
+```js
+const s = require('speculaas');
+const {isBoolean, isString} = s.utils;
+```
--- test/describe.js
+++ test/describe.js
@@ -6,7 +6,10 @@ const s = require('../lib/spec');
 
 describe('Test the describe function', () => {
     
-    it('should describe a ...', () => {
+    it('should describe describe an array', () => {
+        const colors = ['Red', 'Blue', 'Dun'];
+        s.def('::color', colors);
+        expect(s.describe('::color')).to.eql(colors);
     });
 });
 
