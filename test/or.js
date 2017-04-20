bee583f1ac71e37c2dc87e78f5a46e60bce52273
Maurits Rijk <maurits.rijk@ing.nl>
2017-04-20 11:47:16 +0200

    Add test for unform in or

--- test/or.js
+++ test/or.js
@@ -1,23 +1,25 @@
-'use strict';
-
 const _ = require('lodash');
 
 const {expect} = require('chai');;
 
 const s = require('../lib/spec');
 
+const {idemPotent} = require('./utils');
+
 const {isInt, isInteger, isString} = s.utils;
 
 describe('Test the or function', () => {
 
-    s.def('::integer?', s.or(':x', isInteger));
-    s.def('::name-or-id', s.or(':name', isString, ':id', isInt));
+    before(() => {
+        s.def('::integer?', s.or(':x', isInteger));
+        s.def('::name-or-id', s.or(':name', isString, ':id', isInt));
+    });
 
     it('should test the or of 1 spec', () => {
         expect(s.isValid('::integer?', 12)).to.be.true;
         expect(s.isValid('::integer?', 'foobar')).to.be.false;
     });
-    
+
     it('should test the or of 2 specs', () => {
         expect(s.isValid('::name-or-id', 'abc')).to.be.true;
         expect(s.isValid('::name-or-id', 100)).to.be.true;
@@ -27,6 +29,10 @@ describe('Test the or function', () => {
     it('should return tag name and conformed value', () => {
         expect(s.conform('::name-or-id', 'abc')).to.eql([':name', 'abc']);
         expect(s.conform('::name-or-id', 13)).to.eql([':id', 13]);
+    });
+
+    it.only('should unform a conformed value', () => {
+        expect(idemPotent('::name-or-id', 'abc')).to.be.true;
     });
 
     it('should implement a generator', () => {
