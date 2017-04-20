15bc8f9272d76b261d7c3176d4a8f612f95bcccb
Maurits Rijk <maurits.rijk@ing.nl>
2017-04-20 17:01:37 +0200

    Bump version

---
+++ test/isInstInRange.js
@@ -0,0 +1,35 @@
+const {expect} = require('chai');
+
+const s = require('../lib/spec');
+const stest = require('../lib/test');
+
+describe('Test isInstInRange? function', () => {
+    it('should return true if value is within range', () => {
+        const start = new Date(0);
+        const end = new Date('2017');
+        const value = start;
+        expect(s.isInstInRange(start, end, value)).to.be.true;
+    });
+
+    it('should exclude the upper bound', () => {
+        const start = new Date(0);
+        const end = new Date('2017');
+        const value = end;
+        expect(s.isInstInRange(start, end, value)).to.be.false;
+    });
+
+    it('should return false if value is outside the range', () => {
+        const start = new Date('2017');
+        const end = new Date('2018');
+        const value = new Date('2015');
+        expect(s.isInstInRange(start, end, value)).to.be.false;
+    });
+
+    xit('should use the spec to test', () => {
+        const instInRange = s.isInstInRange;
+        const specs = require('../specs/instInRange');
+        s.fdef(instInRange, specs);
+
+        expect(stest.check(instInRange)).to.have.property('result').to.equal(true);        
+    });
+});
