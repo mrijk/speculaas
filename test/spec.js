ca89bd5c54c01ad669baba9c6842b6838f45a680
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-05 09:59:29 +0200

    Updates

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,7 @@
+const _ = require('lodash');
+
 const {expect} = require('chai');
+const testcheck = require('testcheck');
 
 const s = require('../lib/spec');
 
@@ -14,6 +17,15 @@ describe('Test the spec function', () => {
         expect(s.isValid(s.spec(suit), ':spade')).to.be.true;
     });
 
+    it('should create a spec with a generator', () => {
+        const isBit = x => x === 0 | x === 1;
+        const genBit = () => testcheck.gen.intWithin(0, 1);
+
+        s.def('::bit', s.spec(isBit, {gen: genBit}));
+        expect(s.exercise('::bit', 8)).to.have.length(8)
+            .to.satisfy(sample => _.every(sample, ([b]) => isBit(b)))
+    });
+        
     it('should unform a conformed value', () => {
         expect(idemPotent(s.spec(x => x < 42), 13)).to.be.true;
     });
