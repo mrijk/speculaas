efe557ba617552872a514127c4bb1bde4912da0b
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-08 13:40:50 +0200

    Start implementing fspec

--- test/fspec.js
+++ test/fspec.js
@@ -1,12 +1,33 @@
-'use strict';
-
 const {expect} = require('chai');
 
 const s = require('../lib/spec');
+const stest = require('../lib/test');
 
-const {isEven, isInteger} = s.utils;
+const {isInteger} = s.utils;
 
 describe('Test the fspec function', () => {
     it('should create a spec for a function', () => {
+        const adder = x => y => x + y;
+        const fspec = s.fspec({
+            args: s.cat(':y', isInteger),
+            ret: isInteger
+        });
+
+        expect(s.isValid(fspec, adder(2))).to.be.true;
+    });
+
+    xit('should create a spec for a function', () => {
+
+        const adder = x => y => x + y;
+
+        s.fdef(adder, {
+            args: s.cat(':x', isInteger),
+            ret: s.fspec({
+                args: s.cat(':y', isInteger),
+                ret: isInteger
+            })
+        });
+
+        console.log(stest.check(adder));
     });
 });
