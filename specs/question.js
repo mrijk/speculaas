28f34a35ab77008a39da838eacf281135eceec7b
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-08 09:16:07 +0200

    Move predicate generator to new file

--- specs/question.js
+++ specs/question.js
@@ -1,13 +1,5 @@
-const _ = require('lodash');
-
 const s = require('../lib/spec');
-const testcheck = require('testcheck');
-
-const {isInteger, isString, isDate} = s.utils;
-
-// Simple definition of a predicate
-const genPred = () => testcheck.gen.oneOf([isInteger, isDate, isString, s.tuple(isInteger)]);
-const isPred = s.spec(x => _.isFunction(x) || s.isSpec(x), {gen: genPred});
+const isPred = require('./pred');
 
 module.exports = {
     args: s.cat(':pred', isPred),
