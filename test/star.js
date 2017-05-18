97e8131c68d5c44b531ba2874ae20f69a6dfc62f
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-18 16:00:16 +0200

    All tests passing

--- test/star.js
+++ test/star.js
@@ -11,11 +11,11 @@ const {isBoolean, isInteger, isOdd, isString} = s.utils;
 describe('Test the star (*) function', () => {
     s.def('::odd?', s.and(isInteger, isOdd));
     const odds = s.star('::odd?');
-    
-    xit('should return the value', () => {
+
+    it('should return the value', () => {
         expect(s.conform(odds, [1, 3])).to.eql([1, 3]);
     });
-    
+
     it('should accept an empty value sequence', () => {
         expect(s.conform(odds, [])).to.eql([]);
     });
