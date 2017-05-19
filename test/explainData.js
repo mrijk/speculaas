d219b7fe2b13c15fb5dbec98db9e1d18fc0adc4e
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-19 13:25:33 +0200

    Start implementing explain

--- test/explainData.js
+++ test/explainData.js
@@ -5,8 +5,12 @@ const s = require('../lib/spec');
 const {isString, unknownString} = s.utils;
 
 describe('Test the explainData function', () => {
-    it('should return null if succesful', () => {
+    it('should return null if succesful on a predicate function', () => {
         expect(s.explainData(isString, 'foobar')).to.equal(null);
+    });
+
+    it('should return null if succesful on an array', () => {
+        expect(s.explainData([1, 2], 2)).to.equal(null);
     });
 
     it('should return an object explaining why the spec fails', () => {
