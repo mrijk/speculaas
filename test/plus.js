6b2559ec03b939071a3ed0ca0401825a04cc20bb
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-23 11:35:18 +0200

    Minor cleanup

--- test/plus.js
+++ test/plus.js
@@ -13,49 +13,59 @@ describe('Test the plus (+) function', () => {
         s.def('::odd?', s.and(isInteger, isOdd));
         s.def('::odds', s.plus('::odd?'))
     });
-    
-    it('should return the value', () => {
-        expect(s.conform('::odds', [1, 3])).to.eql([1, 3]);
+
+    describe('should handle valid input', () => {
+        it('conform should return the value', () => {
+            expect(s.conform('::odds', [1, 3])).to.eql([1, 3]);
+        });
+
+        it('explainData should return null', () => {
+            expect(s.explainData('::odds', [1, 3])).to.be.null;
+        });
+    });
+
+    describe('should reject empty input', () => {
+        it('conform should return the invalid string when value sequence is empty', () => {
+            expect(s.conform('::odds', [])).to.equal(invalidString);
+        });
+
+        it('explainData should report a problem', () => {
+            expect(s.explainData('::odds', [])).to.eql({
+                problems: [
+                    {
+                        path: [],
+                        reason: 'Insufficient input',
+                        val: [],
+                        via: ['::odds'],
+                        'in': []
+                    }
+                ]
+            });
+        });
     });
     
-    it('should return the invalid string when value sequence is empty', () => {
-        expect(s.conform('::odds', [])).to.equal(invalidString);
-    });
-    
-    it('should return the invalid string', () => {
-        expect(s.conform('::odds', [1, 3, 6])).to.equal(invalidString);
+    describe('should reject invalid input', () => {
+        it('should return the invalid string', () => {
+            expect(s.conform('::odds', [1, 3, 6])).to.equal(invalidString);
+        });
+
+        it('explainData should report a problem', () => {
+            expect(s.explainData('::odds', [1, 3, 6])).to.eql({
+                problems: [
+                    {
+                        path: [],
+                        pred: 'isOdd',
+                        val: 6,
+                        via: ['::odds'],
+                    'in': [2]
+                    }
+                ]
+            });
+        });
     });
 
     it('should unform a conformed value', () => {
         expect(idemPotent('::odds', [1, 3, 5])).to.be.true;
-    });
-
-    it('should implement explain', () => {
-        expect(s.explainData('::odds', [1, 3, 5])).to.be.null;
-
-        expect(s.explainData('::odds', [])).to.eql({
-            problems: [
-                {
-                    path: [],
-                    reason: 'Insufficient input',
-                    val: [],
-                    via: ['::odds'],
-                    'in': []
-                }
-            ]
-        });
-
-        expect(s.explainData('::odds', [1, 3, 6])).to.eql({
-            problems: [
-                {
-                    path: [],
-                    pred: 'isOdd',
-                    val: 6,
-                    via: ['::odds'],
-                    'in': [2]
-                }
-            ]
-        });
     });
 
     it('should implement a generator', () => {
