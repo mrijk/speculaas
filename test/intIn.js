716a8add8bc42b59b7aaff26b33ee756c1ed0d4a
Maurits Rijk <maurits.rijk@ing.nl>
2017-05-24 08:37:58 +0200

    implement explain for intIn

--- test/intIn.js
+++ test/intIn.js
@@ -21,16 +21,46 @@ describe('Test the IntIn function', () => {
         });
     });
 
-    it('should exclude the upper bound', () => {
-        expect(s.isValid('::oneByte', 256)).to.be.false;
-    });
+    describe('should reject invalid input', () => {
+        it('should exclude the upper bound', () => {
+            expect(s.isValid('::oneByte', 256)).to.be.false;
+        });
 
-    it('should return false if value is outside the range', () => {
-        expect(s.isValid('::oneByte', 512)).to.be.false;
-    });
+        it('should return false if value is outside the range', () => {
+            expect(s.isValid('::oneByte', 512)).to.be.false;
+        });
 
-    it('should only accept integers', () => {
-        expect(s.isValid('::oneByte', 3.14)).to.be.false;
+        it('should only accept integers', () => {
+            expect(s.isValid('::oneByte', 3.14)).to.be.false;
+        });
+
+        it('explainData should report about wrong type', () => {
+            expect(s.explainData('::oneByte', 3.14)).to.eql({
+                problems: [
+                    {
+                        path: [],
+                        pred: 'isInt',
+                        val: 3.14,
+                        via: ['::oneByte'],
+                        'in': []
+                    }
+                ]
+            });
+        });
+
+        it('explainData should report about value outside range', () => {
+            expect(s.explainData('::oneByte', 512)).to.eql({
+                problems: [
+                    {
+                        path: [],
+                        pred: 'isIntInRange(0, 256, value)',
+                        val: 512,
+                        via: ['::oneByte'],
+                        'in': []
+                    }
+                ]
+            });
+        });
     });
     
     it('should unform a conformed value', () => {
