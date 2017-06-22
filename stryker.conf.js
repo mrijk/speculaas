67c2a7a311a7cc847c6d07f62d6e0ae0652b94b4
Maurits Rijk <maurits.rijk@ing.nl>
2017-06-22 14:59:53 +0200

    Improve config

---
+++ stryker.conf.js
@@ -0,0 +1,16 @@
+module.exports = function(config) {
+    config.set({
+        files: [
+            { pattern: 'lib/*.js', mutated: true, included: false},
+            { pattern: 'specs/*.js', mutated: false, included: false},
+            { pattern: 'lib/util/*.js', mutated: false, included: false},
+            { pattern: 'lib/test/*.js', mutated: false, included: false},
+            'test/*.js',
+            'test/utils.js'
+        ],
+        testRunner: 'mocha',
+        testFramework: 'mocha',
+        coverageAnalysis: 'perTest',
+        reporter: ['html', 'progress'],
+    });
+}
