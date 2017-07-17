6d0d607615df2dbccaefe26a9234a57c0a763c98
Maurits Rijk <maurits.rijk@ing.nl>
2017-07-17 14:25:38 +0200

    Improve explain function

--- test/explain.js
+++ test/explain.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 
 const s = require('../lib/spec');
 
-const {isString} = s.utils;
+const {isBoolean, isString} = s.utils;
 
 describe('Test the explain function', () => {
     it('should return the explanation string success', () => {
@@ -18,7 +18,14 @@ describe('Test the explain function', () => {
     });
 
     it('should return a string explaining why the spec fails', () => {
-        expect(s.explainStr(isString, 1)).to.have.string('val: 1 fails predicate');
+        expect(s.explainStr(isString, 1)).to.equal('val: 1 fails predicate: isString\n');
     });
+
+    it('should return a string explaining why the spec fails with a specific reason', () => {
+        s.def('::bool-or-string', s.alt(':s', isString, ':b', isBoolean));
+        expect(s.explainStr('::bool-or-string', [])).to.equal('val:  fails spec: ::bool-or-string predicate: ' +
+                                                              'alt,:s,isString,:b,isBoolean' +
+                                                              ', Insufficient input\n');
+    });    
 });
 
