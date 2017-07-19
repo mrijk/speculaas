5bfb60757f81c784b4a03896c544202bc92ef767
Maurits Rijk <maurits.rijk@ing.nl>
2017-07-19 08:46:28 +0200

    Initial code

--- examples/pwdgen.js
+++ examples/pwdgen.js
@@ -2,4 +2,8 @@
 // of http://upgradingdave.com/blog/posts/2016-06-21-random-pwd-with-spec.html
 // by Dave Paroulek
 
+const s = require('../lib/spec');
 
+s.def('::two-lowers', s => s.match(/.*[a-z]+.*[a-z]+.*/));
+s.isValid('::two-lowers', '1234');
+s.isValid('::two-lowers', '12b34a');
