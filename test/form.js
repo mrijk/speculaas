2ed8f8109a7c5a2c759f17041e082465e3858541
Maurits Rijk <maurits.rijk@ing.nl>
2017-08-10 13:50:44 +0200

    Start implementing form

---
+++ test/form.js
@@ -0,0 +1,18 @@
+const {expect} = require('chai');
+
+const s = require('../lib/spec');
+
+const {isInteger} = s.utils;
+
+describe('Test the form function', () => {
+    before(() => {
+        s.def('::a', isInteger);
+    });
+
+    it('should throw an error if spec doesn\'t exist', () => {
+        expect(() => s.form('::foobar')).to.throw(Error, /Unable to resolve spec/);
+    });
+});
+
+ 
+
